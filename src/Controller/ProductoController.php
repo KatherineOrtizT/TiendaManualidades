14f775f7757b6c48a4c502c0d4dfe24363dfdfdb
KatherineOrtizT <raquel.tapiaortiz@educa.madridz.org>
2023-02-16 12:51:47 +0100

    vistas Panel-admin, Contacto, Sobre-Nosotros

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -8,6 +8,7 @@ use App\Repository\ProductoRepository;
 use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -59,7 +60,7 @@ class ProductoController extends AbstractController
             return $this->redirectToRoute('app_homepage_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('producto/new.html.twig', [
+        return $this->render('admin/panel-admin.html.twig', [
             'producto' => $producto,
             'form' => $form,
         ]);
